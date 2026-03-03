7ccfb564df8eafda011f175ddd69dcc3c61f57fe
sabelo-ceza-26 <220094489@mycput.ac.za>
2026-03-03 23:47:01 +0200

    Testing

---
+++ src/test/java/za/ac/cput/Factory/EmployeeFactoryTest.java
@@ -0,0 +1,22 @@
+package za.ac.cput.Factory;
+
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Employee;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class EmployeeFactoryTest {
+
+    @Test
+    void createEmployee(){
+        Employee employee = EmployeeFactory.createEmployee("52222",
+                "Sabie","Ceza","sabeloceza@gmail.com",50000.00f);
+
+        assertNotNull(employee);
+        System.out.println(employee);
+
+    }
+
+
+}
